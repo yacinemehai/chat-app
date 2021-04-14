4b2b80264610123f1565b6885165119703627e70
yacinemehai <yacine.mehai@gmail.com>
2021-04-14 23:28:37 +0200

    props: quest done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact
+      name="Yacine MEHAIGNI"
+      avatar="https://randomuser.me/api/portraits/men/12.jpg"
+      online={true}
+      />
+      <Contact
+      name="Theo Le Basque"
+      avatar="https://randomuser.me/api/portraits/men/80.jpg"
+      online={false}
+      />
+      <Contact
+      name="Anthony Verges"
+      avatar="https://randomuser.me/api/portraits/women/73.jpg"
+      online={true}
+      />
     </div>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Contact.css';
+import PropTypes from 'prop-types';
+
+function Contact({name, avatar, online}) {
+    return (
+        <div className="Contact">
+            <img className="avatar" src={avatar} alt="Yacine MEHAIGNI">
+            </img>
+            <div>
+                <p className="name">{name}</p>
+                <div className="status">
+                    <div className={`${online ? "status-online" : "status-offline"}`} />
+                    <p className="status-text">{online ? 'online' : 'offline'}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  online: PropTypes.bool.isRequired
+
+};
+export default Contact;
